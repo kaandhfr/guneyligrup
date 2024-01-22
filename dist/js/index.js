ed977e75e5b0e8320e95cacfc80d891ad907a041
Kaan Ucar <kaanucr2@gmail.com>
2024-01-23 01:13:12 +0300

    anasayfa hero section slider olarak güncellendi

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,4 +1,13 @@
-const swiper = new Swiper(".blogSwiper", {
+const heroSection = new Swiper("#entry-hero-section", {
+    fadeEffect: { crossFade: true },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    effect: "fade",
+});
+
+const blogSwiper = new Swiper(".blogSwiper", {
     fadeEffect: { crossFade: true },
     pagination: {
         el: ".swiper-pagination",
